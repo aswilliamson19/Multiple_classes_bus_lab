392969fb826a0b6eed28f44f932834e91ed7dcc9
Amanda Williamson <aselliott4@aol.co.uk>
2019-10-08 14:55:27 +0100

    Bus stop added

---
+++ src/test/java/BusStopTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest {
+
+    private BusStop busStop;
+    private Person person;
+
+    @Before
+    public void before(){
+        busStop = new BusStop("Waverley Station");
+        person = new Person();
+    }
+
+    @Test
+    public void getBusStopName() {
+        assertEquals("Waverley Station", busStop.getName());
+    }
+
+    @Test
+    public void canAddPersonToQueue() {
+        busStop.addPerson(person);
+        assertEquals(1, busStop.queueCount());
+    }
+
+}
